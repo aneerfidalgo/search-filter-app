783f19d1e7701a5123c8ead2f5ab1045628f91a3
Aneer Fidalgo <aneer@Aneers-Air.hsd1.fl.comcast.net>
2022-07-13 17:03:16 -0400

    mapping through JSON DATA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import JSONDATA from ".MOCK_DATA.json";
 
 function App() {
   return (
     <div className="App">
       {" "}
       <input type="text" placeholder="search..." />
+      {JSONDATA.map((val, key) => {})}
     </div>
   );
 }
